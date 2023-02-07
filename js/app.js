d84207e7ce5e9b59715258084b0ca62030c2cad2
kimlong0601 <kimlongt40@gmail.com>
2023-02-07 15:27:05 +0700

    update code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-let hero = document.querySelector('.hero');
+let hero = document.querySelector('.hero')
 
-let slider = document.querySelector('.slider');
+let slider = document.querySelector('.slider')
 
-let slide = slider.querySelectorAll('.slide');
+let slides = slider.querySelectorAll('.slide')
 
-let can_slide_play = true;
+let slide_index = 0
 
-let slide_index = 0;
+let can_slide_play = true
+
+
 
 let hero_bgs = [
-    '../assests/images/meat_bg.jpg',
-    '../assests/images/pork.bg.jpg',
-    '../assests/images/salmon_bg.jpg'
-];
+    './assests/images/beef_bg.jpg',
+    './assests/images/pork_bg.jpg',
+    './assests/images/salmon_bg.jpg',
+]
 
 showSlide = (index) => {
     slides.forEach(e => e.classList.remove('active'))
@@ -25,7 +27,34 @@ nextSlide = () => {
     showSlide(slide_index)
 };
 
-showSlide(slide_index);
-// auto play slide
-setInterval(nextSlide, 1500);
+// pause slide item when hover
+slider.addEventListener('mouseover', () => can_slide_play = false)
+// resume slide item when mouse leave
+slider.addEventListener('mouseleave', () => can_slide_play = true)
+
+showSlide(slide_index)
+// // auto play slide
+ setInterval(() => {
+    if (!can_slide_play) return
+    nextSlide()
+}, 5500);
+
+slider.querySelectorAll('.slider-control__item').forEach((item, index) => {
+    item.addEventListener('click', () => showSlide(index))
+})
+
+//menu toggle
+const menuToggle = document.querySelector('#menu-toggle');
+const menu = document.querySelector('.main-menu');
+const menuClose = document.querySelector('#menu-close');
+
+menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+
+menuClose.addEventListener("click", () => {
+    menuToggle.classList.remove("active");
+    menu.classList.remove("active");
+});
 
